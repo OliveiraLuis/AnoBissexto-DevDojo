2adbffaac3a2f9430c7429b755b4d5afc472ca04
OliveiraLuis <oliveira1eduardo2luis3@gmail.com>
2017-09-10 23:48:55 -0300

    Correção do último commit: Passou no teste de 1944 é primo

--- src/dojo/Bissexto.java
+++ src/dojo/Bissexto.java
@@ -4,7 +4,19 @@ public class Bissexto {
 
 	public static boolean definePrimo(int ano) {
 		boolean resposta = false;
-		
+		if(ano%4 == 0) {
+			if(ano%100 == 0) {
+				if(ano%400 == 0) {
+					resposta = true;
+				}else {
+					resposta = false;
+				}
+			}else {
+				resposta = true;
+			}
+		}else {
+			resposta = false; 
+		}
 		return resposta;
 	}
 
